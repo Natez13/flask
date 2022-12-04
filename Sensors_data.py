2c6bd7705dca5b8277cde757d9e1057b63055069
Natez <70248621+Natez13@users.noreply.github.com>
2022-12-04 20:09:48 -0300

    Add files via upload

--- Sensors_data.py
+++ Sensors_data.py
@@ -7,7 +7,6 @@ from datetime import datetime
 from ast import literal_eval
 
 #API's de sensors_data
-
 @app.route('/api/v1/sensor_data/send',methods=['POST'])
 @token_required
 @api_sensor_req
@@ -42,6 +41,25 @@ def send_data(current_sensor_api_key,current_sensor_id,current_user,):
         return resp
 
 
+@app.route('/api/v1/sensor_data_delete/id/',methods=['DELETE'])
+@token_required
+# @api_company_req current_company_api_key,current_company_id,
+def delete_data(current_user):
+    json = request.json
+    sensor_id  = json['sensor_id']
+    from_date = json['from']
+    to_date = json['to']
+    try:
+        sql = f"DELETE FROM Sensor_data WHERE sensor_id={sensor_id} and measure_time BETWEEN {from_date} and {to_date}"
+        conn = connection()
+        conn.execute(sql)
+        conn.commit()
+        conn.close()
+        return make_response('sensors data deleted',200)
+    except:
+        return make_response('sensors not exist or id is invalid',500,)
+
+
 @app.route('/api/v1/sensor_data',methods=['GET'])
 @token_required
 @api_company_req
@@ -51,23 +69,29 @@ def get_data(current_company_api_key,current_company_id,current_user,):
     to_date = request.args.get('to')
     company_id = request.args.get('company_id')
     sensor_id_list = literal_eval(sensor_id)
+    counter = list()
     if int(current_company_id) == int(company_id):
         try:
+            Sensors_data_collention = []
             for i in sensor_id_list:
                 sql = f"SELECT * FROM Sensor_data Where sensor_id={i} and measure_time BETWEEN {from_date} and {to_date}"
                 conn = connection()
                 rv = conn.execute(sql)
                 rows = rv.fetchall()
                 conn.close()
-                Sensors_data_collention = []
-                sensors_data = {}
+
+                Sensors_data_id = []
+                counter.append(sql)
                 for j in rows:
+                    sensors_data = {}
                     sensors_data["sensor_id "] = j["sensor_id"]
                     sensors_data["measure_time "] = datetime.fromtimestamp(j["measure_time"])
                     sensors_data["temperature "] = j["temperature"]
-                    Sensors_data_collention.append(sensors_data)
+                    Sensors_data_id.append(sensors_data)
+                Sensors_data_collention.append(Sensors_data_id)
+                        
 
-            resp = jsonify(Sensors_data_collention)
+            resp = jsonify(Sensors_data_collention,counter)
             resp.status_code = 201  
             return resp
         except:
@@ -76,7 +100,7 @@ def get_data(current_company_api_key,current_company_id,current_user,):
             resp.status_code = 400  
             return resp
     else:
-        resp = jsonify('Sensor id not match with sensor_api_key ')
+        resp = jsonify('Company id not match with company_api_key ')
         resp.status_code = 400  
         return resp
 
